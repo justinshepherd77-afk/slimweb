080d5e81907a46ef4cdca3f2fbbd19499708a0a7
justinshepherd77-afk <justin.shepherd77@gmail.com>
2025-10-16 22:13:35 -0600

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('#country-buttons .btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const c = btn.getAttribute('data-country');
-    alert('Country selected: ' + c + '\n(Next: add city buttons, then summary/categories.)');
-  });
-});
